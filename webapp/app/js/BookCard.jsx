68b5fba2ad163c88476f7cca584bfa17bca06f8b
xmacala <xmacala@fi.muni.cz>
2024-03-14 11:47:53 +0100

    changed BookCard component

--- webapp/app/js/BookCard.jsx
+++ webapp/app/js/BookCard.jsx
@@ -5,19 +5,28 @@ export default function BookCard({ title, author, avg_rating, genres, descriptio
 
   const toggleDescription = () => setShowDescription(!showDescription);
   return (
-    <div className="book-card">
-      <img className="book-cover" src={imageUrl} alt={title} />
-      <div className="info-section">
-        <h3>{title}</h3>
-        <p>by {author}</p>
-        <span>average rating: {avg_rating}</span>
-        <ul>
-          <li key={genres}>{genres}</li>
-        </ul>
-        <button onClick={toggleDescription}>
+    <div className="book-card__container">
+      <div className="book-card__cover">
+        <img className="book-card__cover__image" src={imageUrl} alt={title} />
+      </div>
+
+      <div className="book-card__info-section">
+        <div className="book-card__info-section__genre-section">
+          <span>{genres}</span>
+        </div>
+        <h3 className="book-card__info-section__title">{title}</h3>
+        <p className="book-card__info-section__author">by {author}</p>
+        <div className="book-card__info-section__rating">
+          <i className="fas fa-star"></i>
+          <span>{avg_rating}</span>
+        </div>
+        <button
+          className="book-card__info-section__show-description-btn"
+          onClick={toggleDescription}
+        >
           {showDescription ? 'Hide Description' : 'Show Description'}
         </button>
-        {showDescription && <p className="description">{description}</p>}
+        {showDescription && <p className="book-card__description">{description}</p>}
       </div>
     </div>
   );
