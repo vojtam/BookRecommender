afb9485914fa1fba460587c27c1a01dd9f38c02b
Ubuntu <vojtam@rdevbox.fedora>
2024-03-15 09:38:31 +0100

    added quanteda dependencies

---
+++ webapp/quanteda_dependencies.R
@@ -0,0 +1,9 @@
+rhino::pkg_install("quanteda/quanteda")
+rhino::pkg_install("quanteda/quanteda.corpora")
+rhino::pkg_install("quanteda/readtext")
+rhino::pkg_install("quanteda/quanteda.textplots")
+rhino::pkg_install("quanteda/quanteda.textstats")
+rhino::pkg_install("quanteda/spacyr")
+
+
+
