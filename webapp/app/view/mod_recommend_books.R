944b5b6a2f802dfa40f59b0c09fc5f7cc9897191
vojtam <macala.vojtech@gmail.com>
2024-04-05 18:58:40 +0200

    implemented genres checkbox

--- webapp/app/view/mod_recommend_books.R
+++ webapp/app/view/mod_recommend_books.R
@@ -1,5 +1,6 @@
 box::use(
   shiny[moduleServer, NS, tags, req, div, actionButton, reactiveVal, tagList, observeEvent, uiOutput, renderUI],
+  shinyWidgets[checkboxGroupButtons],
   purrr[pmap],
 
 )
@@ -14,6 +15,23 @@ ui <- function(id) {
   ns <- NS(id)
   div(
     class = "flex-center",
+    
+    checkboxGroupButtons(
+      inputId = ns("genre_selector"),
+      label = "Choose genres to be included:", 
+      choices = c(`<div class=''>fantasy</div>` = "fantasy",
+                  `<span class=''>children</span>` = "children",
+                  `<span class=''>history & biography</span>` = "history_biography",
+                  `<span class=''>comics</span>` = "comics",
+                  `<span class=''>romance</span>` = "romance",
+                  `<span class=''>poetry</span>` = "poetry",
+                  `<span class=''>YA</span>` = "YA",
+                  `<span class=''>crime</span>` = "crime"
+                  ),
+      justified = TRUE,
+    ),
+    
+    
     actionButton(ns("get_recommend_btn"), "Get Recommendations"),
     uiOutput(ns("bookCardsOutput"))
     
@@ -32,6 +50,8 @@ server <- function(id, corp_dfm, query_book_titles, data_tab, how_many, simil_me
     
     observeEvent(gargoyle::watch("start_recommend_event"), {
       req(query_book_titles())
+      print(query_book_titles())
+      browser()
       titles <- get_recommendations(corp_dfm, query_book_titles(), simil_metrics, how_many)
       recommends_tab <- parse_recommendations(titles, data_tab)
       book_recommends_tab(recommends_tab)
